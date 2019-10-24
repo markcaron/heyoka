7794711e4b2accbdfdcdc49ad80e68f2c48c8173
Mark Caron <mcaron@redhat.com>
2019-10-24 10:09:20 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mark Caron :: UX Interaction Designer, Web Developer &amp; Unicorn :: Raleigh, NC</title>
+
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<meta name="description" content="Mark Caron is a web developer, UX interaction designer, and artist living in Raleigh, NC." />
+<meta name="keywords" content="Mark Caron, heyoka, design, heyokadesign, web, development, artist, identity, logos, internet, illustration, photography, paintings, xhtml, css, user experience, ux, ui, raleigh, nc, north carolina, usa" />
+<meta name="classification" content="html5, css, design, web design, ux, user experience, graphic design, artwork, north carolina, us" />
+
+<meta name="author" content="Mark Caron" />
+<meta name="copyright" content="&copy; <?php echo date('Y'); ?>. Mark Caron. All rights reserved." />
+
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="icon" href="favicon.ico" sizes="16x16 32x32" />
+<link rel="mask-icon" href="unicorn.svg" color="#000000">
+
+<link href="css/normalize.css" rel="stylesheet" type="text/css" />
+<link href="css/heyoka.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<article class="page">
+	<header id="masthead" class="masthead">
+		<h1>Mark Caron</h1>
+		<p>UX Interaction Designer, Web Developer &amp; Unicorn</p>
+	</header>
+
+	<section id="about" class="about">
+		<h2>About Me</h2>
+		<p>I'm a Principal User Experience Designer and Developer with experience in leading and mentoring cross-functional engineering teams, and standardizing best practices.</p>
+		<p>I'm passionate about accessibility and creating inclusive user experiences. And tacos.</p>
+		<p class="resume"><a href="mark-caron-resume.html" class="cta">Read my résumé / <abbr title="Curriculum Vitae">CV</abbr></a></p>
+	</section>
+
+	<aside class="social">
+		<h2 id="h-social">Find me on</h2>
+		<ul aria-labelledby="h-social">
+			<li class="linkedin">
+				<a href="http://www.linkedin.com/in/heyoka/" title="LinkedIn" aria-label="Find me on LinkedIn"><svg id="linkedin" viewBox="0 0 512 512"><path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z"/></svg>
+					<span>LinkedIn</span>
+				</a>
+			</li>
+
+			<li class="dribbble">
+				<a href="https://dribbble.com/heyoka" title="Dribbble" aria-label="My portfolio on Dribbble"><svg id="dribbble" viewBox="0 0 512 512"><path d="M255,112c-79,0-143.3,64.2-143.3,143.1S176,398.2,255,398.2S398.3,334,398.3,255.1S334,112,255,112z M349.7,178
+	c17,20.8,27.4,47.3,27.6,76.1c-4-0.8-44.5-9-85.2-3.9c-0.9-2.1-1.7-4.2-2.6-6.3c-2.5-6-5.3-11.9-8.1-17.7
+	C326.6,207.8,347.1,181.5,349.7,178z M255,133.1c31.1,0,59.5,11.6,81.1,30.8c-2.2,3.1-20.6,27.9-64.1,44.1
+	c-20-36.8-42.3-67-45.7-71.5C235.5,134.3,245.1,133.1,255,133.1z M203,144.7c3.2,4.4,25.1,34.7,45.4,70.7
+	c-57.2,15.2-107.7,15-113.1,14.9C143.1,192.3,168.7,160.8,203,144.7z M132.6,255.3c0-1.2,0-2.5,0.1-3.7c5.4,0.1,64.7,0.9,125.8-17.4
+	c3.5,6.8,6.9,13.8,9.9,20.7c-1.6,0.5-3.2,0.9-4.8,1.5c-63.1,20.4-96.7,75.9-99.5,80.6C144.5,315.3,132.6,286.7,132.6,255.3z
+	 M255,377.5c-28.3,0-54.4-9.6-75.1-25.8c2.2-4.5,27-52.3,96-76.3c0.3-0.1,0.5-0.2,0.8-0.3c17.2,44.6,24.3,82,26.1,92.7
+	C288.1,374.1,271.9,377.5,255,377.5z M323.4,356.6c-1.2-7.4-7.8-43.2-23.8-87.1c38.3-6.1,72,3.9,76.2,5.2
+	C370.4,308.7,350.8,338.1,323.4,356.6z"/></svg>
+					<span>Dribbble</span>
+				</a>
+			</li>
+
+			<li class="behance">
+				<a href="http://www.behance.net/heyoka/frame" title="Behance" aria-label="My Behance portfolio"><svg id="behance" viewBox="0 0 512 512"><path d="M254.8 171.8c6.4 8.9 9.6 19.6 9.6 32 0 12.8-3.2 23.1-9.7 30.9 -3.6 4.4-9 8.4-16 12 10.7 3.9 18.8 10.1 24.2 18.5 5.5 8.4 8.2 18.7 8.2 30.7 0 12.4-3.1 23.6-9.3 33.5 -4 6.6-8.9 12.1-14.9 16.5 -6.7 5.1-14.6 8.6-23.7 10.5 -9.1 1.9-19 2.8-29.6 2.8H99.1V149.5h101.4C226.1 149.8 244.2 157.3 254.8 171.8zM140.9 185.9v46.3h51c9.1 0 16.5-1.7 22.2-5.2 5.7-3.5 8.5-9.6 8.5-18.4 0-9.8-3.8-16.2-11.3-19.4 -6.5-2.2-14.7-3.3-24.8-3.3H140.9zM140.9 266.9v55.9h50.9c9.1 0 16.2-1.2 21.2-3.7 9.2-4.6 13.8-13.3 13.8-26.2 0-10.9-4.5-18.4-13.4-22.5 -5-2.3-12-3.5-21-3.6H140.9L140.9 266.9zM396.4 207.3c10.8 4.8 19.8 12.5 26.8 23 6.4 9.2 10.5 19.9 12.4 32.1 1.1 7.1 1.5 17.4 1.3 30.8H323.9c0.6 15.6 6 26.5 16.2 32.7 6.2 3.9 13.6 5.8 22.4 5.8 9.2 0 16.8-2.4 22.5-7.1 3.2-2.6 5.9-6.1 8.4-10.7h41.4c-1.1 9.2-6.1 18.5-15 28 -13.9 15.1-33.4 22.6-58.4 22.6 -20.6 0-38.9-6.4-54.6-19.1 -15.8-12.7-23.7-33.4-23.7-62.1 0-26.9 7.1-47.5 21.4-61.8 14.2-14.3 32.7-21.5 55.5-21.5C373.4 200 385.6 202.4 396.4 207.3zM335.7 242.3c-5.7 5.9-9.3 13.9-10.8 24h69.9c-0.7-10.8-4.3-18.9-10.8-24.5 -6.5-5.6-14.5-8.4-24.1-8.4C349.5 233.4 341.4 236.4 335.7 242.3zM402.8 161.5h-91.2V182.7h91.2V161.5z"/></svg>
+					<span>Behance</span>
+				</a>
+			</li>
+
+			<li class="instagram">
+				<a href="https://www.instagram.com/raleightasty/" title="Instagram" aria-label="Follow me on Instagram"><svg id="instagram" viewBox="0 0 512 512"><g><path d="M256,135.39c39.28,0,43.94,0.15,59.45,0.86c14.34,0.65,22.13,3.05,27.32,5.07
+		c6.87,2.67,11.77,5.86,16.92,11.01c5.15,5.15,8.34,10.05,11.01,16.92c2.01,5.18,4.41,12.97,5.07,27.32
+		c0.71,15.51,0.86,20.17,0.86,59.45s-0.15,43.94-0.86,59.45c-0.65,14.34-3.05,22.13-5.07,27.32c-2.67,6.87-5.86,11.77-11.01,16.92
+		c-5.15,5.15-10.05,8.34-16.92,11.01c-5.18,2.01-12.97,4.41-27.32,5.07c-15.51,0.71-20.16,0.86-59.45,0.86
+		c-39.29,0-43.94-0.15-59.45-0.86c-14.34-0.65-22.13-3.05-27.32-5.07c-6.87-2.67-11.77-5.86-16.92-11.01
+		c-5.15-5.15-8.34-10.05-11.01-16.92c-2.01-5.18-4.41-12.97-5.07-27.32c-0.71-15.51-0.86-20.17-0.86-59.45s0.15-43.94,0.86-59.45
+		c0.65-14.34,3.05-22.13,5.07-27.32c2.67-6.87,5.86-11.77,11.01-16.92c5.15-5.15,10.05-8.34,16.92-11.01
+		c5.18-2.01,12.97-4.41,27.32-5.07C212.06,135.54,216.72,135.39,256,135.39 M256,108.88c-39.96,0-44.97,0.17-60.66,0.89
+		c-15.66,0.71-26.35,3.2-35.71,6.84c-9.67,3.76-17.88,8.79-26.06,16.97c-8.18,8.18-13.21,16.38-16.97,26.06
+		c-3.64,9.36-6.12,20.05-6.84,35.71c-0.72,15.69-0.89,20.7-0.89,60.66c0,39.96,0.17,44.97,0.89,60.66
+		c0.71,15.66,3.2,26.35,6.84,35.71c3.76,9.67,8.79,17.88,16.97,26.06c8.18,8.18,16.38,13.21,26.06,16.97
+		c9.36,3.64,20.05,6.12,35.71,6.84c15.69,0.72,20.7,0.89,60.66,0.89c39.96,0,44.97-0.17,60.66-0.89c15.66-0.71,26.35-3.2,35.71-6.84
+		c9.67-3.76,17.88-8.79,26.06-16.97c8.18-8.18,13.21-16.38,16.97-26.06c3.64-9.36,6.12-20.05,6.84-35.71
+		c0.72-15.69,0.89-20.7,0.89-60.66s-0.17-44.97-0.89-60.66c-0.71-15.66-3.2-26.35-6.84-35.71c-3.76-9.67-8.79-17.88-16.97-26.06
+		c-8.18-8.18-16.38-13.21-26.06-16.97c-9.36-3.64-20.05-6.12-35.71-6.84C300.97,109.05,295.96,108.88,256,108.88L256,108.88z"/><path d="M256,180.45c-41.72,0-75.55,33.82-75.55,75.55s33.82,75.55,75.55,75.55s75.55-33.82,75.55-75.55
+		S297.72,180.45,256,180.45z M256,305.04c-27.08,0-49.04-21.96-49.04-49.04s21.96-49.04,49.04-49.04
+		c27.08,0,49.04,21.96,49.04,49.04S283.08,305.04,256,305.04z"/><circle cx="334.53" cy="177.47" r="17.65"/></g></svg>
+					<span>Instagram</span>
+				</a>
+			</li>
+
+			<li class="medium">
+				<a href="https://medium.com/@heyoka" title="Medium.com" aria-label="My writings on Medium"><svg id="medium" viewBox="0 0 512 512"><g><path d="M411.4,171.7c-0.1,0-0.1-0.1-0.2-0.1l-0.1-0.1l-94.3-47.2c-0.6-0.3-1.3-0.5-2-0.7
+		c-0.9-0.2-1.7-0.4-2.6-0.4c-3.6,0-7.2,1.8-9.2,5l-54.3,88.3l68.2,110.8l94.8-154.1C412,172.6,411.9,172,411.4,171.7z"/><polygon points="218.8,196.4 218.8,296.8 308.1,341.5 	"/><path d="M321.5,348.2L395,385c9.6,4.8,17.3,1.4,17.3-7.5V200.6L321.5,348.2z"/><path d="M203.3,171.8l-92.1-46.1c-1.7-0.8-3.2-1.2-4.7-1.2c-4,0-6.9,3.1-6.9,8.4v198.9
+		c0,5.3,3.9,11.6,8.7,14l81.1,40.6c2.1,1,4.1,1.5,5.9,1.5c5,0,8.6-3.9,8.6-10.5V172.7C203.9,172.3,203.7,172,203.3,171.8z"/></g></svg>
+					<span>Medium.com</span>
+				</a>
+			</li>
+
+			<li class="nounproject">
+				<a href="https://thenounproject.com/heyoka/uploads/" title="TheNounProject.com" aria-label="My icons on The Noun Project">
+					<svg viewBox="0 0 160 160">
+					<g>
+						<path d="M27.1,62.5h-0.118c-4.674,0.018-9.062,1.855-12.354,5.173S9.532,75.393,9.55,80.067s1.855,9.062,5.173,12.355
+							C18.025,95.698,22.4,97.5,27.049,97.5c0.022,0,0.045,0,0.068,0c4.675-0.018,9.062-1.855,12.355-5.173
+							c3.292-3.318,5.096-7.721,5.078-12.395C44.513,70.357,36.693,62.537,27.1,62.5z"/>
+						<rect x="111.5" y="60.5" width="35" height="35"/>
+						<polygon points="90.818,60.818 78,73.636 65.182,60.818 58.818,67.182 71.636,80 58.818,92.818 65.182,99.182 78,86.364
+							90.818,99.182 97.182,92.818 84.364,80 97.182,67.182 	"/>
+					</g>
+					</svg>
+					<span>The Noun Project</span>
+				</a>
+			</li>
+
+		</ul>
+
+
+	</aside>
+
+		<footer id="site-info" class="site-info">
+			<small>Copyright &copy; <?php echo date('Y'); ?> Mark Caron. All Rights Reserved.</small>
+		</footer>
+
+</article>
+
+</body>
+</html>
