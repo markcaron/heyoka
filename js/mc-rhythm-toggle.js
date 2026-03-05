c9200b04ea5d8e7be314f679a2df45b34948edeb
Mark Caron <mark.caron@gmail.com>
2026-03-05 11:21:39 -0500

    added bricklink, updated soccer blog icon, design tweak to rhythm toggle

--- js/mc-rhythm-toggle.js
+++ js/mc-rhythm-toggle.js
@@ -41,6 +41,7 @@ export default class McRhythmToggle extends HTMLElement {
                 color: var(--color-text, #151515);
                 border: var(--border-width, 1px) solid var(--color-border, #4d4d4d);
                 border-radius: var(--ui-border-radius, 3px);
+                box-shadow: inset 0 0 2px rgba(0,0,0,.3);
                 font-size: var(--mc-scheme-select-font-size, var(--font-size, 1rem));
                 line-height: var(--mc-scheme-select-light-height, var(--line-height, 1rem)); 
                 transition: background 0.1s ease-out;
@@ -59,7 +60,14 @@ export default class McRhythmToggle extends HTMLElement {
                   height: 100%;
                   width: 50%;
                   background: var(--color-tile-bg-hover, #d93f0f);
-                  transition: left 0.1s ease-out;
+                  box-shadow: inset 0 1px 1px rgba(255,255,255,.4), 1px 0 1px rgba(0,0,0,.2);
+                  border-inline-end: var(--border-width, 1px) solid var(--color-border, #4d4d4d);
+                  border-inline-start: 0;
+                  border-start-start-radius: calc(var(--ui-border-radius, 3px) - var(--border-width, 1px));
+                  border-end-start-radius: calc(var(--ui-border-radius, 3px) - var(--border-width, 1px));
+                  border-start-end-radius: 0;
+                  border-end-end-radius: 0;
+                  transition: all 0.1s ease-out;
                 }
 
                 & > #off {
@@ -93,6 +101,13 @@ export default class McRhythmToggle extends HTMLElement {
                   & > #slide {
                     inset-inline-start: 50%;
                     background: var(--color-link, #0066cc);
+                    box-shadow: inset 0 1px 1px rgba(255,255,255,.4), -1px 0 1px rgba(0,0,0,.2);
+                    border-inline-end: 0;
+                    border-inline-start: var(--border-width, 1px) solid var(--color-border, #4d4d4d);
+                    border-start-start-radius: 0;
+                    border-end-start-radius: 0;
+                    border-start-end-radius: calc(var(--ui-border-radius, 3px) - var(--border-width, 1px));
+                    border-end-end-radius: calc(var(--ui-border-radius, 3px) - var(--border-width, 1px));
                   }
 
                   & > #on {
